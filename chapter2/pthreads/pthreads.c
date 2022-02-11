d281ea6eb837692805c656fbb18eb433073db33c
Ryo Katsuse <ryokatsu0719@gmail.com>
2022-02-11 21:20:30 +0900

    fix:スレッドの処理を実装

--- chapter2/pthreads/pthreads.c
+++ chapter2/pthreads/pthreads.c
@@ -16,3 +16,25 @@ void *thread_func(void *arg) {
     return "finished!"; // 返り値
 }
 
+int main(int argc, char *argv[]) {
+  pthread_t v[NUM_THREADS];
+  // スレッド生成
+  for (int i = 0; i < NUM_THREADS; i++){
+    if (pthread_create(&v[i], NULL, thread_func, (void *)i) != 0) {
+      perror("pthread_create");
+      return -1;
+    }
+  }
+  // スレッドの終了を待機
+  for (int i = 0; i < NUM_THREADS; i++){
+    char *ptr;
+    if(pthread_join(v[i], (void **)&ptr) == 0) {
+      printf("msg = %s\n" , ptr);
+    } else {
+      perror("pthread_create");
+      return -1;
+    }
+  }
+  return 0;
+}
+
